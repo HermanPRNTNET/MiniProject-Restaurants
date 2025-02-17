8795ea4c0221555243ebaa4209231be672b0c23f
Herman Pranoto <hermanprnt.net@gmail.com>
2025-02-17 14:57:09 +0700

    check point API Controller

---
+++ Restaurants.Domain/Repositories/IRestaurantRepository.cs
@@ -0,0 +1,14 @@
+﻿using Restaurants.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Domain.Repositories
+{
+    public interface IRestaurantRepository
+    {
+        Task<IEnumerable<Restaurant>> GetAllAsync();
+    }
+}
---
+++ Restaurants.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,26 @@
+﻿using Restaurants.Infrastructure.Presistence;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Restaurants.Infrastructure.Seeders;
+using Restaurants.Domain.Repositories;
+using Restaurants.Infrastructure.Repositories;
+
+namespace Restaurants.Infrastructure.Extensions; 
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddInfrastructure(this IServiceCollection services,IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("RestaurantsDb");
+        services.AddDbContext<RestaurantsDbContext>(options => options.UseSqlServer(connectionString));
+
+        services.AddScoped<IRestaurantSeeder, RestaurantSeeder>();
+        services.AddScoped<IRestaurantRepository, RestaurantRepository>();
+    }
+}
