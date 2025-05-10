fa2fbd3fdd2ab75e04ef4661e81aaa30d614b290
Herman Pranoto <hermanprnt.net@gmail.com>
2025-05-11 00:25:27 +0700

    middleware test

---
+++ Restaurants.API.Tests/Middlewares/ErrorHandlingMiddlewareTests.cs
@@ -0,0 +1,63 @@
+﻿using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Restaurants.Domain.Exceptions;
+using FluentAssertions;
+
+namespace Restaurants.API.Middlewares.Tests;
+
+public class ErrorHandlingMiddlewareTests
+{
+    [Fact()]
+    public async Task InvokeAsync_WhenNoExceptionThrown_ShouldCallNextDelegate()
+    {
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleWare = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var nextDelegate = new Mock<RequestDelegate>();
+
+        await middleWare.InvokeAsync(context, nextDelegate.Object); 
+
+        nextDelegate.Verify(n => n.Invoke(context), Times.Once);
+    }
+
+
+    [Fact()]
+    public async Task InvokeAsync_WhenNotFoundExceptionThrown_ShouldSetStatusCode404()
+    {
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleWare = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var notFoundException = new NotFoundException(nameof(Restaurants),"1");
+
+        await middleWare.InvokeAsync(context, _ => throw notFoundException);
+
+        context.Response.StatusCode.Should().Be(404);
+    }
+
+    [Fact()]
+    public async Task InvokeAsync_WhenForbidenExceptionThrown_ShouldSetStatusCode403()
+    {
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleWare = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var exception = new ForbidenException();
+
+        await middleWare.InvokeAsync(context, _ => throw exception);
+
+        context.Response.StatusCode.Should().Be(403);
+    }
+    [Fact()]
+    public async Task InvokeAsync_WhenInternalServerError_ShouldSetStatusCode500()
+    {
+        var loggerMock = new Mock<ILogger<ErrorHandlingMiddleware>>();
+        var middleWare = new ErrorHandlingMiddleware(loggerMock.Object);
+        var context = new DefaultHttpContext();
+        var exception = new SystemException();
+
+        await middleWare.InvokeAsync(context, _ => throw exception);
+
+        context.Response.StatusCode.Should().Be(500);
+    }
+}
---
+++ Restaurants.Infrastructure.Tests/Authorization/Requirements/CreatedMultipleRestaurantRequirementHandlerTests.cs
@@ -0,0 +1,95 @@
+﻿using Xunit;
+using Restaurants.Application.Users;
+using Moq;
+using Restaurants.Domain.Entities;
+using Restaurants.Domain.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using FluentAssertions;
+
+namespace Restaurants.Infrastructure.Authorization.Requirements.Tests;
+
+public class CreatedMultipleRestaurantRequirementHandlerTests
+{
+    [Fact()]
+    public async Task HandleRequirementAsync_UserHasCreatedMultipleRestaurants_ShouldSucceed()
+    {
+        //arrange
+
+        var currentUser = new CurrentUser("1", "test@mailc.com", [],"Indonesian",null);
+        var userContextMoq = new Mock<IUserContext>();
+        userContextMoq.Setup(u => u.GetCurrentUser()).Returns(currentUser);
+
+        var restaurants = new List<Restaurant>()
+        {
+            new()
+            {
+                OwnerId = currentUser.Id,
+            },
+            new()
+            {
+                OwnerId = currentUser.Id,
+            },
+            new()
+            {
+                OwnerId = "2",
+            }
+        };
+
+        var restaurantsRepositoryMock = new Mock<IRestaurantsRepository>();
+        restaurantsRepositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(restaurants);
+
+        var requirement = new CreatedMultipleRestaurantRequirement(2);
+        var handler = new CreatedMultipleRestaurantRequirementHandler(restaurantsRepositoryMock.Object, userContextMoq.Object);
+
+        var context = new AuthorizationHandlerContext([requirement],null,null);
+
+        //act
+        await handler.HandleAsync(context); 
+
+
+        //asert
+        context.HasSucceeded.Should().BeTrue();
+
+
+    }
+
+    [Fact()]
+    public async Task HandleRequirementAsync_UserHasNotCreatedMultipleRestaurants_ShouldFail()
+    {
+        //arrange
+
+        var currentUser = new CurrentUser("1", "test@mailc.com", [], "Indonesian", null);
+        var userContextMoq = new Mock<IUserContext>();
+        userContextMoq.Setup(u => u.GetCurrentUser()).Returns(currentUser);
+
+        var restaurants = new List<Restaurant>()
+        {
+            new()
+            {
+                OwnerId = currentUser.Id,
+            },
+            new()
+            {
+                OwnerId = "2",
+            }
+        };
+
+        var restaurantsRepositoryMock = new Mock<IRestaurantsRepository>();
+        restaurantsRepositoryMock.Setup(r => r.GetAllAsync()).ReturnsAsync(restaurants);
+
+        var requirement = new CreatedMultipleRestaurantRequirement(2);
+        var handler = new CreatedMultipleRestaurantRequirementHandler(restaurantsRepositoryMock.Object, userContextMoq.Object);
+
+        var context = new AuthorizationHandlerContext([requirement], null, null);
+
+        //act
+        await handler.HandleAsync(context);
+
+
+        //asert
+        context.HasSucceeded.Should().BeFalse();
+        context.HasFailed.Should().BeTrue();
+
+
+    }
+}
